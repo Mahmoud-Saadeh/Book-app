ce051c5fc3f02e096a6d681c0bc032e67e669c32
Mahmoud Saadeh <mahmoud.saadeh998@gmail.com>
2021-05-31 17:47:10 +0300

    added mongoDB data 2

--- src/app.js
+++ src/app.js
@@ -15,20 +15,20 @@ class App extends React.Component {
     return (
       <>
         <Router>
-          {/* <IsLoadingAndError> */}
-          <Header />
-          <Switch>
-            <Route exact path="/">
-              {/* TODO: if the user is logged in, render the `MyFavoriteBooks` component, if they are not, render the `Login` component */}
-              {isAuthenticated ? <MyFavoriteBooks /> : <Login />}
-            </Route>
-            <Route exact path="/user">
-              {/* TODO: add a route with a path of '/profile' that renders a `Profile` component */}
-              <User />
-            </Route>
-          </Switch>
-          <Footer />
-          {/* </IsLoadingAndError> */}
+          <IsLoadingAndError>
+            <Header />
+            <Switch>
+              <Route exact path="/">
+                {/* TODO: if the user is logged in, render the `MyFavoriteBooks` component, if they are not, render the `Login` component */}
+                {isAuthenticated ? <MyFavoriteBooks /> : <Login />}
+              </Route>
+              <Route exact path="/user">
+                {/* TODO: add a route with a path of '/profile' that renders a `Profile` component */}
+                <User />
+              </Route>
+            </Switch>
+            <Footer />
+          </IsLoadingAndError>
         </Router>
       </>
     );
